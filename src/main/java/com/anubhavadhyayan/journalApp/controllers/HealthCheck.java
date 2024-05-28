66d6887a00864e508f2f45a8afa35ec7f4d2b3b3
anubhavadhyayan <anubhavadhyayan@gmail.com>
2024-05-28 16:16:23 +0530

    Api check route added

--- src/main/java/com/anubhavadhyayan/journalApp/controllers/HealthCheck.java
+++ src/main/java/com/anubhavadhyayan/journalApp/controllers/HealthCheck.java
@@ -3,11 +3,28 @@ package com.anubhavadhyayan.journalApp.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 public class HealthCheck {
 
     @GetMapping("/health-check")
     public String healthCheck(){
-        return "Ok";
+        return "API is working fine.";
+    }
+
+    @GetMapping("/anu")
+    public String printOwnerName() {
+        return "Anubhav Adhyayan";
+    }
+
+    private final Map<String, Object> apiHealthCheck = new HashMap<>();
+    @GetMapping("/api_check")
+    public Map<String, Object> apiCheck(){
+        apiHealthCheck.put("status", true);
+        apiHealthCheck.put("message", "Api is working fine.");
+        return apiHealthCheck;
     }
 }
